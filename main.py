7755fa092ea59a58a6408eea1407e898e78689f9
Alexander Maxwell <alexandermaxwell@Alexanders-MBP.attlocal.net>
2020-05-27 19:41:32 -0500

    new updates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageColor
 
 
 # img.getpixel((x/y))
@@ -9,40 +9,39 @@ from PIL import Image, ImageDraw
 
 img = Image.open("pic.jpg")
 width, height = img.size
-img2 = Image.new('RGB',(width,height),0)
+img2 = Image.new('RGB',(width,height),(255,255,255))
 percent = 0
 
+#img2.putpixel((50,50),(50,50,50,255))
 #test = img.getpixel(50, 50)
 #print(test)
-"""
-for x in range(img.size[0]):
-  for y in range(img.size[1]):
-    #print(img.getpixel((x,y)))
-    with open("imgval.txt", "a") as file:
-        file.write(str(img.getpixel((x,y))))
 
-file.close()
 
-"""
+
+
 for w in range(width):
     for h in range(height):
         percent+=1
+        img2.putpixel((h,w),(50,50,50,255))
+        print(percent/(width*height))
 
+    print("---------------------------------------------------------------")
+"""
         if(w<20):
-            img2.ImageDraw.ImageDraw.point((h,w),(img.getpixel((h,w)),img.getpixel((h,w)),img.getpixel((h,w+20))))
+            img2.putpixel((h,w),50,50,50)
         #    img2.putpixel((h,w), 50,50,50)
         #    img2.putpixel((h,w),        )
 
         if(w<20):
 
-            img2.ImageDraw.point((h,w-20),(img.getpixel((h,w)),img.getpixel((h,w)),img.getpixel((h,w))))
+            img2.Draw.point((h,w-20),(img.getpixel((h,w)),img.getpixel((h,w)),img.getpixel((h,w))))
         else:
 
-            img2.ImageDraw.point((h,w-20),(img.getpixel((h,w)),img.getpixel((h,w)),img.getpixel((h,w+20))))
+            img2.Draw.point((h,w-20),(img.getpixel((h,w)),img.getpixel((h,w)),img.getpixel((h,w+20))))
 
         percent+=1
-        print(percent/(width*height))
-    print("---------------------------------------------------------------")
+"""
+
 
 
 img2.save('glitchpic.jpg','JPEG')
