df97dd423d8ddf3d66dec29bae5050c503ad5c63
root <root@DESKTOP-ATQ3L5T.localdomain>
2020-06-01 23:06:46 -0500

    vertical randomness incorporated

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from PIL import Image, ImageColor
 from tqdm import tqdm
+from random import random
 
 
+
+print("name of file :\n")
 imagename = input()
 try :
     img = Image.open("input/" + imagename)
@@ -9,8 +12,9 @@ try :
     img2 = Image.new('RGB',(width + 1,height + 1),(255,255,255))
     percent = 0
     print(img.size)
-    offset = 5e-02*width
+    offset = (random() * .1 * width)
     for w in tqdm(range(width)):
+        offset = (random() * .05 * width)
         for h in range(height):
 
             if(w<offset):
@@ -19,7 +23,7 @@ try :
                 img2.putpixel((w,h),(img.getpixel((w-offset,h))[0],img.getpixel((w,h))[1],img.getpixel((w,h))[2]))
             else:
                 img2.putpixel((w,h),(img.getpixel((w-offset,h))[0],img.getpixel((w,h))[1],img.getpixel((w+offset,h))[2]))
-    img2.save("output/" + imagename,'JPEG')
+    img2.save("output/test_" + imagename ,'JPEG')
 except:
     print("error: request could not be processed")
 
