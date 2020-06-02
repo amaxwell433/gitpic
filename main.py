b2bc77e7f8f42362c586ec6ea144d203b86cccc8
root <root@DESKTOP-ATQ3L5T.localdomain>
2020-06-01 22:39:44 -0500

    functioning update

--- main.py
+++ main.py
@@ -2,49 +2,27 @@ from PIL import Image, ImageColor
 from tqdm import tqdm
 
 
-# img.getpixel((x/y))
-# puts out rgb values
-# 2 layer for loop get average color
-# find value on white to dark green
-#
+imagename = input()
+try :
+    img = Image.open("input/" + imagename)
+    width, height = img.size
+    img2 = Image.new('RGB',(width + 1,height + 1),(255,255,255))
+    percent = 0
+    print(img.size)
+    offset = 5e-02*width
+    for w in tqdm(range(width)):
+        for h in range(height):
 
-img = Image.open("pic.jpg")
-width, height = img.size
-img2 = Image.new('RGB',(width + 1,height + 1),(255,255,255))
-percent = 0
+            if(w<offset):
+                img2.putpixel((w,h),(img.getpixel((w,h))[0],img.getpixel((w,h))[1],img.getpixel((w+offset,h))[2]))
+            if(w>offset):
+                img2.putpixel((w,h),(img.getpixel((w-offset,h))[0],img.getpixel((w,h))[1],img.getpixel((w,h))[2]))
+            else:
+                img2.putpixel((w,h),(img.getpixel((w-offset,h))[0],img.getpixel((w,h))[1],img.getpixel((w+offset,h))[2]))
+    img2.save("output/" + imagename,'JPEG')
+except:
+    print("error: request could not be processed")
 
-#img2.putpixel((50,50),(50,50,50,255))
-#test = img.getpixel(50, 50)
-#print(test)
-#r = img.getpixel((50,50))[0]
-#print(r)
-
-
-w = 0
-h = 0
-for w in tqdm(range(3,width)):
-    for h in range(3,height):
-        #percent+=1
-        #img2.putpixel((int(h/2),w),(w,h,50,255))
-        #print(percent/(width*height))
-
-
-        if(w<20):
-            img2.putpixel((h+1,w+1),(img.getpixel((h,w))[0],img.getpixel((h,w))[1],img.getpixel((h,w))[2]))
-        if(w<20):
-            img2.putpixel((h,w),(img.getpixel((h,w))[0],img.getpixel((h,w))[1],img.getpixel((h,w))[2]))
-        else:
-            img2.putpixel((h,w),(img.getpixel((h,w))[0],img.getpixel((h,w))[1],img.getpixel((h,w))[2]))
-
-
-
-img2.save('glitchpic.jpg','JPEG')
-
-
-
-#print(col)
-print(width)
-print(height)
 
 
 # lorem ipsum code to do
